c3ab22fa12c4e48701097cf8f18130032a35a264
csibvili <csibi.vili@gmail.com>
2017-09-13 14:27:51 +0200

    new sweetsgit add ./

---
+++ week-02/day-03/CandyShop.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CandyShop
+{
+    public class Candyshop
+    {
+        public static void Main(string[] args)
+        {
+            var list = new List<object>();
+            list.Add("Cupcake");
+            list.Add(2);
+            list.Add("Brownie");
+            list.Add(false);
+
+            // Accidentally we added "2" and "false" to the list. 
+            // Your task is to change from "2" to "Croissant" and change from "false" to "Ice cream"
+            // No, don't just remove the lines
+
+            list[list.IndexOf(2)] = "Croissant".ToString();
+            list[list.IndexOf(false)] = "Ice Cream".ToString();
+
+            foreach (var sweet in list)
+            {
+                Console.WriteLine(sweet);
+            }
+            Console.ReadLine();
+        }
+    }
+}
