4dad17c5da6d04d603644655a410ff5e3678765e
csibvili <csibi.vili@gmail.com>
2017-09-13 15:36:27 +0200

    creating while loops to find the words

---
+++ week-02/day-03/QuoteSwap.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QuoteSwap
+{
+    public class QuoteSwap
+    {
+        public static void Main(string[] args)
+        {
+            var list = new List<string> { "What", "I", "do", "create,", "I", "cannot", "not", "understand." };
+            int i = 0;
+            int j = 0;
+            // Accidentally I messed up this quote from Richard Feynman.
+            // Two words are out of place
+            // Your task is to fix it by swapping the right words with code
+
+            // Also, print the sentence to the output with spaces in between.
+
+            //   string tmp = list[list.FindIndex("do")];
+            //   list[list.FindIndex("do")] = list[list.FindIndex("cannot")];
+            //   list[list.FindIndex("cannot")] = tmp;
+
+            /*string tmp = list[2];
+            list[2] = list[5];
+            list[5] = tmp;*/
+
+            while (list[i] == "do")
+            {
+                i++;
+            }
+
+            while (list[j] == "cannot")
+            {
+                j++;
+            }
+
+            string tmp = list[i];
+            list[i] = list[j];
+            list[j] = tmp;
+
+            foreach (var word in list)
+            {
+                Console.Write(word + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
