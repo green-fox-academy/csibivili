0a3e0aec4e65d96c7d8e22a51550ae57fc669d1f
csibvili <csibi.vili@gmail.com>
2017-09-13 12:52:52 +0200

    creating a todo list via special charachters

---
+++ week-02/day-03/ToDoPrint.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoPrint
+{
+    public class TodoPrint
+    {
+        public static void Main(string[] args)
+        {
+            string todoText = " - Buy milk\n";
+            // Add "My todo:" to the beginning of the todoText
+            // Add " - Download games" to the end of the todoText
+            // Add " - Diablo" to the end of the todoText but with indention
+
+            // Expected output:
+
+            // My todo:
+            //  - Buy milk
+            //  - Download games
+            //      - Diablo
+
+            todoText = todoText + " - Download games\n\t - Diablo";
+            Console.WriteLine(todoText);
+            Console.ReadLine();
+        }
+    }
+}
