f8ffbabaae0dfa091a4b229f9b0c2d53921338ce
csibvili <csibi.vili@gmail.com>
2017-09-13 16:23:00 +0200

    creating userinputs interface and type conversation

---
+++ week-02/day-03/Calculator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        public static void Main(string[] args)
+        {
+            // Create a simple calculator application which reads the parameters from the prompt
+            // and prints the result to the prompt.
+            // It should support the following operations:
+            // +, -, *, /, % and it should support two operands.
+            // The format of the expressions must be: {operation} {operand} {operand}.
+            // Examples: "+ 3 3" (the result will be 6) or "* 4 4" (the result will be 16)
+
+            // You can use the Scanner class
+            // It should work like this:
+
+            // Start the program
+            // It prints: "Please type in the expression:"
+            // Waits for the user input
+            // Print the result to the prompt
+            // Exit
+
+            // UserInput(string what, int n1, int n2);
+
+            string what;
+
+            
+            Console.WriteLine("Please type in the expression: (i.e.: + 3 3)");
+            what = Console.ReadLine();
+            string[] userinputs = what.Split(' ');
+
+            /*foreach (var userinput in userinputs)
+            {
+                Console.WriteLine(userinput);
+            }*/
+            int num1 = Int32.Parse(userinputs[1]);
+            int num2 = Int32.Parse(userinputs[2]);
+
+            Console.WriteLine(num1 + num2);
+
+            Console.ReadLine();
+        }    
+
+        /*public static void UserInput (string what, int n1 , int n2)
+        {
+            what = Console.ReadLine();
+            Console.Write(" ");
+            n1 = Console.Read();
+            Console.Write(" ");
+            n2 = Console.Read();
+        }*/
+
+    }
+}
