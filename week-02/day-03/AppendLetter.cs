3576352439dd4df6e502075171351dfe8bdb0f48
csibvili <csibi.vili@gmail.com>
2017-09-13 14:06:11 +0200

    adding 'a's to the end of the words

---
+++ week-02/day-03/AppendLetter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AppendLetter
+{
+    public class AppendLetter
+    {
+        public static void Main(string[] args)
+        {
+            var far = new List<string> { "kuty", "macsk", "kacs", "rók", "halacsk" };
+            // Add "a" to every string in the far list.
+
+            for (int i = 0; i < far.Count; i++)
+            {
+                //Console.WriteLine(far[i] + "a");
+                far[i] = String.Concat(far[i], "a");
+                Console.WriteLine(far[i]);
+            }
+
+            /*foreach (var animal in far)
+            {
+                Console.WriteLine(animal);
+            }*/
+            //Console.WriteLine(far);
+            Console.ReadLine();
+        }
+    }
+}
