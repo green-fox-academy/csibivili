ab81bc1f4c199a8025cf59c3e341923f3fe7450a
csibvili <csibi.vili@gmail.com>
2017-09-13 13:38:06 +0200

    missing Saturn has been added

---
+++ week-02/day-03/SolarSystem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolarSystem
+{
+    public class SolarSystem
+    {
+        public static void Main(string[] args)
+        {
+            var planetList = new List<string> { "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Uranus",
+            "Neptune" };
+
+
+            // Saturn is missing from the planetList
+            // Insert it into the correct position
+
+            planetList.Insert(5,"Saturn");
+
+            foreach (var planet in planetList)
+            {
+                Console.WriteLine(planet);
+            }
+
+            //Console.WriteLine(planetList);
+
+            Console.ReadLine();
+        }
+    }
+}
