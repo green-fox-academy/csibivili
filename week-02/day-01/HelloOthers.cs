fc77b9d4b5a7f22584213901d6e16df50cdb115c
Csibi Vili <csibi.vili@gmail.com>
2017-09-13 22:25:09 +0200

    replace some stuff

---
+++ week-02/day-01/HelloOthers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Greet 3 of your classmates with this program, in three separate lines
+            // like:
+            //
+            Console.WriteLine("Hello, Balázs!");
+            Console.WriteLine("Hello, Lóri!");
+            Console.WriteLine("Hello, Albert!");
+
+
+            // Hello, Mary!
+            // Hello, Joe!
+        }
+    }
+}
---
+++ week-02/day-01/TwoNumbers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a program that prints a few operations on two numbers: 22 and 13
+
+            // Print the result of 13 added to 22
+
+            // Print the result of 13 substracted from 22
+
+            // Print the result of 22 mutiplied by 13
+
+            // Print the result of 22 divided by 13 (as a decimal fraction)
+
+            // Print the integer part of 22 divided by 13
+
+            // Print the reminder of 22 divided by 13
+            float a = 22;
+            float b = 13;
+            Console.WriteLine(a + b);
+            Console.WriteLine(a - b);
+            Console.WriteLine(a * b);
+            Console.WriteLine(a / b);
+            Console.WriteLine((a / b).ToString("0"));
+            Console.WriteLine(a - b);
+            Console.ReadLine();
+        }
+    }
+}
