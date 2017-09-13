5ba79956126fda6d27ec270aefc686504fe4efb0
csibvili <csibi.vili@gmail.com>
2017-09-13 13:53:44 +0200

    cupido

---
+++ week-02/day-03/Matchmaking.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace Matchmaking
+{
+    public class Matchmaking
+    {
+        public static void Main(string[] args)
+        {
+            var girls = new List<string> { "Eve", "Ashley", "Bözsi", "Kat", "Jane" };
+            var boys = new List<string> { "Joe", "Fred", "Béla", "Todd", "Neef", "Jeff" };
+            var order = new List<string>();
+
+            // Join the two lists by matching one girl with one boy in the order list
+            // Exepected output: "Eve", "Joe", "Ashley", "Fred"...
+
+            for (int i = 0; i < girls.Count; i++)
+            {
+                order.Add(string.Concat(girls[i] ," , ", boys[i]));
+                //Console.WriteLine(string.Concat(girls[i], " , ", boys[i]));
+            }
+
+              
+            foreach (var couple in order)
+            {
+                Console.WriteLine(couple);
+            }
+
+            //Console.WriteLine(order);
+            Console.ReadLine();
+        }
+    }
+}
