f79fb73209c51794b9459e9598facd83baa4574b
Csibi Vili <csibi.vili@gmail.com>
2017-09-13 21:41:31 +0200

    rename

---
+++ week-02/day-01/Exec06.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exec_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double avgdailycodin = 6;
+            double semesterweeks = 17;
+            double avgweeklyworkinghours = 52;
+            double totalhours , percentage;
+            // double percentage;
+
+            totalhours = 5 * semesterweeks * avgdailycodin;
+
+            percentage = (totalhours / (avgweeklyworkinghours * semesterweeks)) * 100;
+          
+            Console.WriteLine(totalhours);
+
+            Console.WriteLine(percentage);
+
+            Console.ReadLine();
+        }
+    }
+}
