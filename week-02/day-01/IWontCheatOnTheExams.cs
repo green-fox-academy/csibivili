72010e68e82cbfce497d7cedf7d86f2b617fc32b
csibvili <csibi.vili@gmail.com>
2017-09-14 13:41:03 +0200

    i will

---
+++ week-02/day-01/IWontCheatOnTheExams.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a program that writes this line 100 times:
+            // "I won't cheat on the exam!"
+
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("I won't cheat on the exam!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
