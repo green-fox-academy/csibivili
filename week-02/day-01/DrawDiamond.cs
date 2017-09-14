09c82d93da7cef8af1e5f5bc453e843fe30b20f6
csibvili <csibi.vili@gmail.com>
2017-09-14 16:42:13 +0200

    my brain has suffered some damage

---
+++ week-02/day-01/DrawDiamond.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that reads a number from the standard input, then draws a
+            // diamond like this:
+            //
+            //
+            //    *
+            //   ***
+            //  *****
+            // *******
+            //  *****
+            //   ***
+            //    *
+            //
+            // The diamond should have as many lines as the number was
+
+            Console.Write("How big diamond do you want? ");
+            string userInput = Console.ReadLine();
+            int triangleRow = int.Parse(userInput);
+
+
+            for (int i = 0; i <= triangleRow; i = i + 2)
+            {
+                for (int k = (triangleRow/2); k > (i / 2); k--)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+
+            }
+
+            for (int i = (triangleRow - 2); i >= 0; i = i -2)
+            {
+                for (int k = i - 2; (k*2) <= (triangleRow + 2); k = k + 2)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
