91dd10db93e00fbfb7f343fe4c43df2ec208b887
csibvili <csibi.vili@gmail.com>
2017-09-14 17:07:36 +0200

    way more easier than the diamond

---
+++ week-02/day-01/DrawSquare.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that reads a number from the standard input, then draws a
+            // square like this:
+            //
+            //
+            // %%%%%
+            // %   %
+            // %   %
+            // %   %
+            // %   %
+            // %%%%%
+            //
+            // The square should have as many lines as the number was
+
+            string userInput = Console.ReadLine();
+            int size = int.Parse(userInput);
+
+            for (int i = 0; i < size; i++)
+            {
+                if (i == 0)
+                {
+                    for (int j = 0; j < size; j++)
+                    {
+                        Console.Write("%");
+                    }
+                }
+                
+                if ((i > 0) & (i <size - 1))
+                {
+                    Console.Write("%");
+                    for (int j = 0; j < (size - 2); j++)
+                    {
+                        Console.Write(" ");
+                    }
+                    Console.Write("%");
+                }
+
+                if (i == size - 1)
+                {
+                    for (int j = 0; j < size; j++)
+                    {
+                        Console.Write("%");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
