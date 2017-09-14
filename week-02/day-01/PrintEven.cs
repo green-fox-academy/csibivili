e18b11c1ab53e6bb1a0560bc8a9c4ad77894902d
csibvili <csibi.vili@gmail.com>
2017-09-14 13:44:16 +0200

    fantastic for loops everywhere

---
+++ week-02/day-01/PrintEven.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a program that prints all the even numbers between 0 and 500
+            for (int i = 0; i <= 500; i = i+2)
+            {
+                Console.Write(i + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
