af5ac95a6eaa5dca33a13fe30ac848272993fbfe
csibvili <csibi.vili@gmail.com>
2017-09-14 15:33:35 +0200

    that was hard enough

---
+++ week-02/day-01/FizzBuzz.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that prints the numbers from 1 to 100.
+            // But for multiples of three print “Fizz” instead of the number
+            // and for the multiples of five print “Buzz”.
+            // For numbers which are multiples of both three and five print “FizzBuzz”.
+
+            for (int i = 1; i < 101; i++)
+            {
+                if (i % 15 == 0)
+                {
+                    Console.Write("FizzBuzz ");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.Write("Buzz ");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.Write("Fizz ");
+                }
+                else
+                {
+                    Console.Write(i + " ");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
