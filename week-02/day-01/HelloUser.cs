83296ad695fcce0f0c2f17e4798c82eb13c848a7
csibvili <csibi.vili@gmail.com>
2017-09-14 10:50:41 +0200

    Hello, Vili!

---
+++ week-02/day-01/HelloUser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Modify this program to greet user instead of the World!
+            // The program should ask for the name of the user
+            string name;
+
+            Console.Write("Please write your name here: ");
+            name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name + "!");
+            Console.ReadLine();
+
+        }
+    }
+}
