57a8f4a6366395f127cbefc544213513a4ffd701
csibvili <csibi.vili@gmail.com>
2017-09-14 11:53:05 +0200

    odd-even-odd-even-even-even-odd-odd...

---
+++ week-02/day-01/OddEven.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that reads a number form the standard input,
+            // Than prints "Odd" if the number is odd, or "Even" it it is even.
+
+            string userinput;
+
+            Console.WriteLine("Give me a number and I let you know if it is even or odd.");
+            userinput = Console.ReadLine();
+            int number = int.Parse(userinput);
+
+            string result = (number % 2 == 0) ? "even" : "odd";
+
+            Console.WriteLine("The number is " + result + ".");
+            Console.ReadLine();
+
+        }
+    }
+}
