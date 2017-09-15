cc64f6187038fdb91a3262540eea4684f4675f1f
csibvili <csibi.vili@gmail.com>
2017-09-15 11:32:06 +0200

    fix(anagram): letterbreakdown can only deal with two words :*(

--- week-02/day-05/Anagram.cs
+++ week-02/day-05/Anagram.cs
@@ -10,42 +10,45 @@ namespace GreenFox
     {
 
         //static string[] letters;
-        static int NumberOfWords = 1;
+        //static int NumberOfWords = 1;
         static string[] array;
-        static int i = 0;
-
+        //static int i = 0;
+        //static List<Array> WordsInLetters = new List<Array>();
+        static string word1 = "";
+        static string word2 = "";
 
         static void Main(string[] args)
         {
             Greetings("Hello!");
-            LetterBreakDown(array[i]);
+            LetterBreakDown(word1);
+
             Console.ReadLine();
         }
 
         static void Greetings (string hello)
         {
-            Console.WriteLine("Hello, my friend! Give me X words and I let you know if they are anagrams of each other");
+            Console.WriteLine("Hello, my friend! Give me 2 words and I let you know if they are anagrams of each other");
             Console.WriteLine();
             Console.WriteLine("Please write here your words divided by one space: ");
             string tmp = Console.ReadLine();
             array = tmp.Split(' ');
-            NumberOfWords = array.Length;
+            //NumberOfWords = array.Length;
+            word1 = array[0];
+            word2 = array[1];
         }
 
         static void LetterBreakDown(string word)
         {
+            
 
-            char[] charArray = array[i].ToCharArray();
+            char[] charArray1 = array[0].ToCharArray();
+            char[] charArray2 = array[1].ToCharArray();
 
-            /*foreach (var word in words)
-            {
-                Console.WriteLine(word);
-            }*/
 
-            for (int i = 0; i < charArray.Length; i++)
-            {
-                Console.WriteLine(charArray[i]);
-            }
+            //for (int j = 0; j < array.Length; j++)
+            //{
+            //    WordsInLetters[j] = new int [2] { 1,2};
+            //}
         }
 
         static void Anagram()
