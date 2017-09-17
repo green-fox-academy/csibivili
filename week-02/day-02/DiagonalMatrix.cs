96e5740105e7e6a92fc105704264a59124551527
Csibi Vili <csibi.vili@gmail.com>
2017-09-17 16:07:39 +0200

    feat(matrix): draw a diagonal matrix

---
+++ week-02/day-02/DiagonalMatrix.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiagonalMatrix
+{
+    class Program
+    {
+        static int size = 6;
+        static int[,] array = new int [size,size];
+
+
+
+        static void Main(string[] args)
+        {
+            // - Create (dynamically) a two dimensional array
+            //   with the following matrix. Use a loop!
+            //
+            //   1 0 0 0
+            //   0 1 0 0
+            //   0 0 1 0
+            //   0 0 0 1
+            //
+            // - Print this two dimensional array to the output
+
+            fill(size);
+            Console.ReadLine();
+        }
+
+        static void fill (int value)
+        {
+            for (int i = 0; i < value; i++)
+            {
+                for (int j = 0; j < value; j++)
+                {
+                    int zeroOrOne = i == j ? 1 : 0;
+                    array[i, j] = zeroOrOne;
+                    Console.Write(zeroOrOne + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
