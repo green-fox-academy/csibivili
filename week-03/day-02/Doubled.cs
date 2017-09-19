d79e26c2dbe04c8ee8ea84410361529039c246b5
csibvili <csibi.vili@gmail.com>
2017-09-19 15:55:48 +0200

    complete Doubled exercise

--- week-03/day-02/Doubled.cs
+++ week-03/day-02/Doubled.cs
@@ -1,1 +1,41 @@
-// Create a method that decrypts the duplicated-chars.txt 
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace NameFromEmail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Create a method that decrypts the duplicated-chars.txt }
+            Console.WriteLine(FixText(IntoChars("duplicated-chars.txt")));
+            Console.ReadLine();
+        }
+
+        static List<string> IntoChars(string fileName)
+        {
+            var charachters = new List<string>();
+            string text = File.ReadAllText(fileName);
+            char[] chars = text.ToCharArray();
+            for (int i = 0; i < chars.Length; i++)
+            {
+                charachters.Add(chars[i].ToString());
+            }
+            return charachters;
+        }
+        static string FixText(List<string> charachters)
+        {
+            string text = "";
+            for (int i = 1; i < charachters.Count; i++)
+            {
+                if (charachters[i -1] != charachters[i])
+                {
+                    text = text + charachters[i];
+                }
+            }
+            return text;
+        }
+    }
+}
