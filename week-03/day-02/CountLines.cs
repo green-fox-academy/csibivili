7a960589672bcea47ee5c8341baced43aaf69c8b
csibvili <csibi.vili@gmail.com>
2017-09-19 12:53:18 +0200

    feat(countlines): it can count the number of lines in a text file

--- week-03/day-02/CountLines.cs
+++ week-03/day-02/CountLines.cs
@@ -1,4 +1,44 @@
-// Write a function that takes a filename as string,
-// then returns the number of lines the file contains.
-// It should return zero if it can't open the file, and
-// should not raise any error.
+using System;
+using System.IO;
+
+namespace NameFromEmail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Write a function that takes a filename as string,
+            // then returns the number of lines the file contains.
+            // It should return zero if it can't open the file, and
+            // should not raise any error.
+            Console.WriteLine(fileName("my-files.txt"));
+            Console.ReadLine();
+
+        }
+
+        static string fileName(string value)
+        {
+            //string path = Path.GetFullPath(value);
+            //return path;
+            int lineCount = 0;
+            try
+            {
+                using (var reader = File.OpenText(value))
+                {
+                    while (reader.ReadLine() != null)
+                    {
+                        lineCount++;
+                    }
+                }
+            }
+            catch (IOException)
+            {
+                Console.Write("");
+            }
+            
+            string lines = lineCount.ToString();
+            return lines;
+        }
+    }
+}
