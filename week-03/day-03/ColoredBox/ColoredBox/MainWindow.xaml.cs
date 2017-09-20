74537bc37cf6e47a25b8e2313304dd1f539e069e
csibvili <csibi.vili@gmail.com>
2017-09-20 11:44:53 +0200

    feat(coloredbox): draw a box in one color

--- week-03/day-03/ColoredBox/ColoredBox/MainWindow.xaml.cs
+++ week-03/day-03/ColoredBox/ColoredBox/MainWindow.xaml.cs
@@ -24,6 +24,44 @@ namespace ColoredBox
             InitializeComponent();
             var foxDraw = new FoxDraw(canvas);
             // draw a box that has different colored lines on each edge.
+            //int x, y, w, y;
+            int x = 10;
+            int y = 60;
+            var colors = new List<string>() { "Red", "Green", "Blue", "Yellow" };
+
+            //var point1 = new Point(x, x);
+            //var point2 = new Point(y, x);
+            //var point3 = new Point(y, y);
+            //var point4 = new Point(x, y);
+
+            //foxDraw.DrawLine(point1, point2);
+            //foxDraw.DrawLine(point2, point3);
+            //foxDraw.DrawLine(point3, point4);
+            //foxDraw.DrawLine(point4, point1);
+
+            DrawSquare(CreatePoints(x, y), colors);
+
         }
+
+        public List<Point> CreatePoints (int x, int y)
+        {
+            var points = new List<Point>();
+            points.Add(new Point(x, x));
+            points.Add(new Point(y, x));
+            points.Add(new Point(y, y));
+            points.Add(new Point(x, y));
+            return points;
+        }
+
+        public void DrawSquare (List<Point> points, List<string> colors)
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            for (int i = 1; i < points.Count; i++)
+            {
+                foxDraw.DrawLine(points[i-1], points[i]);
+            }
+        }
+
     }
 }
