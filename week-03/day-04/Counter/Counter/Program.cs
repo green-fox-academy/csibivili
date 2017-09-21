790784aac3523cf0a31056122e37c291ade0c836
csibvili <csibi.vili@gmail.com>
2017-09-21 10:12:48 +0200

    it can count from N to 0

---
+++ week-03/day-04/Counter/Counter/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 100;
+            Console.WriteLine(CountDown(n));
+            Console.ReadLine();
+        }
+        static int CountDown(int n)
+        {
+            if (n == 0)
+                return 0;
+            else
+            return CountDown(n - 1);
+        }
+    }
+}
