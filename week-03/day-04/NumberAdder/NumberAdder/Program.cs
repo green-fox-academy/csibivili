89c68aad63003ed32d5f74b2721f8cbc8fa45bda
csibvili <csibi.vili@gmail.com>
2017-09-21 11:00:50 +0200

    feat(numberadder): sum all the values from N to 0

---
+++ week-03/day-04/NumberAdder/NumberAdder/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberAdder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 64;
+            Console.WriteLine(Sum(n));
+            Console.ReadLine();
+        }
+
+        static int Sum(int value)
+        {
+            if (value >= 0)
+            {
+                return value + Sum(value - 1);
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
