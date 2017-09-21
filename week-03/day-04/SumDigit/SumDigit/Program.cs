b806fb03980d4f8d4a4eba216970f06c5d626950
csibvili <csibi.vili@gmail.com>
2017-09-21 11:13:04 +0200

    static vs public is not clear

---
+++ week-03/day-04/SumDigit/SumDigit/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumDigit
+{
+    class Program
+    {
+        static int counter = 1;
+
+        static void Main(string[] args)
+        {
+
+            // Given a non-negative int n, return the sum of its digits recursively (no loops). 
+            // Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while 
+            // divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
+
+            Console.WriteLine(NumberOfDigits(1111));
+            Console.ReadLine();
+                
+        }
+
+        static int NumberOfDigits (int value)
+        {
+            if (value / 10 > 0)
+            {
+                counter++;
+                return NumberOfDigits(value / 10);               
+            }
+            else
+            {
+                return counter;
+            }
+        }
+    }
+}
