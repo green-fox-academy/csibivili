b413910224533700f59339a304e25b169e1428c3
csibvili <csibi.vili@gmail.com>
2017-09-21 17:22:34 +0200

    that is not my day

---
+++ week-03/day-04/Exer11/Exer11/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace Exer11
+{
+
+
+    public partial class MainWindow : Window
+    {
+        public Rectangle Square = new Rectangle();
+        public readonly Random randonGen = new Random();
+        public int yPosition = 600;
+        public int xPosition = 600;
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            DrawSquare(foxDraw, Size(200), Size(200), Size(200));
+        }
+
+
+        public int Size(int value)
+        {
+            
+            if (value > 10)
+            {
+                return value /= 3;
+            }
+            else
+            {
+                return value;
+            }
+        }
+
+        public void DrawSquare(FoxDraw foxDraw, int x, int y, int size)
+        {
+            foxDraw.DrawRectangle(x, y, size, size);
+        }
+
+        public Color RandomColor()
+        {
+            Color randomColor = Color.FromArgb(Convert.ToByte(randonGen.Next(255)), Convert.ToByte(randonGen.Next(255)),
+                Convert.ToByte(randonGen.Next(255)), Convert.ToByte(randonGen.Next(255)));
+            return randomColor;
+        }
+    }
+}
---
+++ week-03/day-04/String1/String1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String1
+{
+    class Program
+    {
+        static string xy = "yyyxxxkkkkyxyx";
+        static List<string> charList = new List<string>();
+
+        static void Main(string[] args)
+        {
+
+            // Given a string, compute recursively (no loops) a new string where all the
+            // lowercase 'x' chars have been changed to 'y' chars.
+            Console.WriteLine(ChangeXY(CharBreakDown("xy")));
+            Console.ReadLine();
+
+        }
+
+        static List<string> CharBreakDown (string text)
+        {
+            var list = new List<string>();
+            char[] chars = text.ToCharArray();
+            for (int i = 0; i < chars.Length; i++)
+            {
+                charList.Add(chars[i].ToString());
+            }
+            return charList;
+        }
+
+        static string ChangeXY (List<string> list)
+        {
+            int i = 1;
+            if (list[i].Equals("x") & list[i-1].Equals("y"))
+            {
+                list[i] = "y";
+                list[i - 1] = "x";
+                return list.ToString();
+            }
+            else
+            {
+                return list.ToString();
+            }
+        }
+    }
+}
