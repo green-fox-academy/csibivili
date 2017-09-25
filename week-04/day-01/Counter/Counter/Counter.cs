3e18c031224882a3ff9befaf76024e954c6b59b0
csibvili <csibi.vili@gmail.com>
2017-09-25 22:38:57 +0200

    getting harder

---
+++ week-04/day-01/Counter/Counter/Counter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Counter
+{
+    public class Counter
+    {
+        private int value = 0;
+        private int initialvalue = 0;
+
+        public Counter (int value)
+        {
+            this.value = value;
+            initialvalue = value;
+        }
+
+        public Counter ()
+        {
+
+        }
+
+        public void Add ()
+        {
+            this.value++;
+        }
+
+        public string Get ()
+        {
+            return this.value.ToString();
+        }
+
+        public void Reset ()
+        {
+            this.value = initialvalue;
+        }
+    }
+}
---
+++ week-04/day-01/Counter/Counter/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var counter = new Counter(100);
+
+            counter.Add();
+            Console.WriteLine(counter.Get());
+            counter.Reset();
+            
+        }
+    }
+}
