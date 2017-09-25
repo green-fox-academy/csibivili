3526726af3e1bf7167913b122289c4b76d390d1b
csibvili <csibi.vili@gmail.com>
2017-09-25 22:17:32 +0200

    i found the easy exercises which i missed earlier

---
+++ week-04/day-01/Sharpie/Sharpie/Sharpie.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sharpie
+{
+    public class Sharpie
+    {
+        private string color;
+        private float width;
+        private float inkamount = 100;
+
+        public Sharpie (string color, float width)
+        {
+            this.color = color;
+            this.width = width;
+        }
+
+        public void Use()
+        {
+            inkamount--;
+        }
+    }
+}
