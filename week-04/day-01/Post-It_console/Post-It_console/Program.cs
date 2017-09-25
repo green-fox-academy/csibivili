d3524d0462baa4af89d9341bc3cd1296aff502da
csibvili <csibi.vili@gmail.com>
2017-09-25 13:10:05 +0200

    complete first exercise

---
+++ week-04/day-01/Post-It_console/Post-It_console/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Post_It_console
+{
+    class Program
+    {
+        public struct PostIt
+        {
+            public ConsoleColor Background;
+            public string Text;
+            public ConsoleColor Textcolor;
+
+        }
+
+        static void Main(string[] args)
+        {
+            var postit1 = new PostIt();
+
+            postit1.Background = ConsoleColor.DarkRed;
+            postit1.Text = "Idea 1";
+            postit1.Textcolor = ConsoleColor.Blue;
+
+            Console.BackgroundColor = postit1.Background;
+            Console.ForegroundColor = postit1.Textcolor;
+            Console.WriteLine(postit1.Text);
+
+            var postit2 = new PostIt();
+
+            postit2.Background = ConsoleColor.Magenta;
+            postit2.Text = "Awesome";
+            postit2.Textcolor = ConsoleColor.Black;
+
+            Console.BackgroundColor = postit2.Background;
+            Console.ForegroundColor = postit2.Textcolor;
+            Console.WriteLine(postit2.Text);
+
+            var postit3 = new PostIt();
+
+            postit3.Background = ConsoleColor.Yellow;
+            postit3.Text = "Superb";
+            postit3.Textcolor = ConsoleColor.Green;
+
+            Console.BackgroundColor = postit3.Background;
+            Console.ForegroundColor = postit3.Textcolor;
+            Console.WriteLine(postit3.Text);
+
+
+            Console.ReadLine();
+        }
+    }
+}
