0a60ee0c040994bb9b623f2224255d26f80f3618
csibvili <csibi.vili@gmail.com>
2017-09-25 13:19:30 +0200

    dont create constructor in structs! bad example!

--- week-04/day-01/Post-It_console/Post-It_console/Program.cs
+++ week-04/day-01/Post-It_console/Post-It_console/Program.cs
@@ -14,35 +14,42 @@ namespace Post_It_console
             public string Text;
             public ConsoleColor Textcolor;
 
+            public PostIt (ConsoleColor Background, string Text, ConsoleColor Textcolor)
+            {
+                this.Background = Background;
+                this.Text = Text;
+                this.Textcolor = Textcolor;
+            }
+
         }
 
         static void Main(string[] args)
         {
-            var postit1 = new PostIt();
+            var postit1 = new PostIt(ConsoleColor.DarkRed, "Idea 1", ConsoleColor.Blue);
 
-            postit1.Background = ConsoleColor.DarkRed;
-            postit1.Text = "Idea 1";
-            postit1.Textcolor = ConsoleColor.Blue;
+            //postit1.Background = ConsoleColor.DarkRed;
+            //postit1.Text = "Idea 1";
+            //postit1.Textcolor = ConsoleColor.Blue;
 
             Console.BackgroundColor = postit1.Background;
             Console.ForegroundColor = postit1.Textcolor;
             Console.WriteLine(postit1.Text);
 
-            var postit2 = new PostIt();
+            var postit2 = new PostIt(ConsoleColor.Magenta, "Awesome", ConsoleColor.Black);
 
-            postit2.Background = ConsoleColor.Magenta;
-            postit2.Text = "Awesome";
-            postit2.Textcolor = ConsoleColor.Black;
+            //postit2.Background = ConsoleColor.Magenta;
+            //postit2.Text = "Awesome";
+            //postit2.Textcolor = ConsoleColor.Black;
 
             Console.BackgroundColor = postit2.Background;
             Console.ForegroundColor = postit2.Textcolor;
             Console.WriteLine(postit2.Text);
 
-            var postit3 = new PostIt();
+            var postit3 = new PostIt(ConsoleColor.Yellow,"Superb",ConsoleColor.Green);
 
-            postit3.Background = ConsoleColor.Yellow;
-            postit3.Text = "Superb";
-            postit3.Textcolor = ConsoleColor.Green;
+            //postit3.Background = ConsoleColor.Yellow;
+            //postit3.Text = "Superb";
+            //postit3.Textcolor = ConsoleColor.Green;
 
             Console.BackgroundColor = postit3.Background;
             Console.ForegroundColor = postit3.Textcolor;
