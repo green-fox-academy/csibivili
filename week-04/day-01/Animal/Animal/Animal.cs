61a8e5c4e6894101ccbb4d57ade54cc979f02eae
csibvili <csibi.vili@gmail.com>
2017-09-25 22:07:09 +0200

    i see where we are going

---
+++ week-04/day-01/Animal/Animal/Animal.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal
+{
+    public class Animal
+    {
+        private int hunger = 50;
+        private int thirst = 50;
+
+        public Animal(int hunger, int thirst)
+        {
+            this.hunger = hunger;
+            this.thirst = thirst;
+        }
+
+        public void Eat ()
+        {
+            hunger--;
+        }
+
+        public void Drink ()
+        {
+            thirst--;
+        }
+
+        public void Play ()
+        {
+            hunger++;
+            thirst++;
+        }
+    }
+}
