ae058a8b9b2d837cd9e8389826de6a8d4e3b207d
csibvili <csibi.vili@gmail.com>
2017-09-27 13:02:04 +0200

    first test passed, two empty strings are anagrams of each others, surprise, surprise

---
+++ week-04/day-03/Anagram/Anagram/IfAnagram.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anagram
+{
+    public class IfAnagram
+    {
+        public string IfAnagrams(string input)
+        {
+            return input;
+        }
+    }
+}
---
+++ week-04/day-03/Anagram/AnagramTest/IfAnagramTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Anagram;
+
+namespace AnagramTest
+{
+    [TestFixture]
+    public class IfAnagramTest
+    {
+        IfAnagram anagram = new IfAnagram();
+        [Test]
+        public void TestIfAnagramWhenEmptyStrings ()
+        {
+            string input = String.Empty;
+            string output = String.Empty;
+
+            Assert.AreEqual(output, anagram.IfAnagrams(input));
+        }
+    }
+}
