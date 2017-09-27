62c592b8c22804ccf15535b57cb3cdfef70de856
csibvili <csibi.vili@gmail.com>
2017-09-27 17:01:02 +0200

    easy!

--- week-04/day-03/CowsAndBullsApp/CowsAndBullsApp/CowsAndBulls.cs
+++ week-04/day-03/CowsAndBullsApp/CowsAndBullsApp/CowsAndBulls.cs
@@ -8,28 +8,41 @@ namespace CowsAndBullsApp
 {
     public class CowsAndBulls
     {
+        static readonly Random RANDOM = new Random();
+        static readonly string[] COWBULL = new string[] { "cow", "bull" };
+
         public string CAB(int input)
         {
-            string result = input.ToString();
-            char[] digits = result.ToCharArray();
-            //var textDigits = new List<string>();
-            result = String.Empty;
+            //string result = input.ToString();
+            //char[] digits = result.ToCharArray();
+            //result = String.Empty;
 
-            foreach (var digit in digits)
+            //foreach (var digit in digits)
+            //{
+            //    if (digit == '1')
+            //    {
+            //        result += "cow";
+            //    }
+            //    else
+            //    {
+            //        result += digit.ToString();
+            //    }
+            //}
+            return Match(input);
+        }
+
+        static int RandomNumber ()
+        {
+            return 1111;
+        }
+
+        static string Match(int input)
+        {
+            if (input == RandomNumber())
             {
-                if (digit == '1')
-                {
-                    result += "cow";
-                }
-                else
-                {
-                    result += digit.ToString();
-                }
-                
+                return String.Format("{0}{0}{0}{0}", COWBULL[0]);
             }
-
-
-            return result;
+            return String.Empty;
         }
     }
 }
