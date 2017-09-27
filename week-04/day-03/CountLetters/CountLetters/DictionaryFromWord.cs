5980f192b58de90e700dfdc2e4de4c50433cb60a
csibvili <csibi.vili@gmail.com>
2017-09-27 14:26:51 +0200

    it works with empty string

---
+++ week-04/day-03/CountLetters/CountLetters/DictionaryFromWord.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountLetters
+{
+    public class DictionaryFromWord
+    {
+        public Dictionary<int,char> DictFromWord(string input)
+        {
+            Dictionary<int, char> dictionary = new Dictionary<int, char>();
+            return dictionary;
+        }
+    }
+}
---
+++ week-04/day-03/CountLetters/CountLettersTest/DictionaryFromWordTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CountLetters;
+
+namespace CountLettersTest
+{
+    [TestFixture]
+    public class DictionaryFromWordTest
+    {
+        DictionaryFromWord dictionary = new DictionaryFromWord();
+
+        [Test]
+        public void TestDictFromWordWhenEmptyString ()
+        {
+            string input = String.Empty;
+            Dictionary<int,char> output = new Dictionary<int,char>();
+
+            Assert.AreEqual(output, dictionary.DictFromWord(input));
+        }
+    }
+}
