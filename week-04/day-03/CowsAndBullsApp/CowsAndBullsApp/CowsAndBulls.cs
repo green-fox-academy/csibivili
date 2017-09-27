b414e01bdc875d185375a97a755fcf0ef11da290
csibvili <csibi.vili@gmail.com>
2017-09-27 16:34:08 +0200

    creata environment

---
+++ week-04/day-03/CowsAndBullsApp/CowsAndBullsApp/CowsAndBulls.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CowsAndBullsApp
+{
+    public class CowsAndBulls
+    {
+        public string CAB(int input)
+        {
+            return "cowcowcowcow";
+        }
+    }
+}
---
+++ week-04/day-03/CowsAndBullsApp/CowsAndBullsAppTest/CowsAndBullsTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CowsAndBullsApp;
+using NUnit.Framework;
+
+namespace CowsAndBullsAppTest
+{
+    [TestFixture]
+    public class CowsAndBullsTest
+    {
+        CowsAndBulls cowsAndBulls = new CowsAndBulls();
+
+        [Test]
+        public void TestCABWhen1111()
+        {
+            int input = 1111;
+            string output = "cowcowcowcow";
+
+            Assert.AreEqual(output, cowsAndBulls.CAB(input));
+        }
+    }
+}
