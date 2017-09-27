75b3ce808169c21742bd55772107ce1bcd1c55b0
csibvili <csibi.vili@gmail.com>
2017-09-27 11:52:35 +0200

    not crystal clear yet

---
+++ week-04/day-03/Apple/AppleTest/ApplesTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Apple;
+
+namespace AppleTest
+{
+    [TestFixture]
+    public class ApplesTest
+    {
+        Apples apple = new Apples();
+
+        [Test]
+        public void TestApple_ReturnApple ()
+        {
+            Assert.AreEqual("apple", apple.PrintApple());
+        }
+    }
+}
