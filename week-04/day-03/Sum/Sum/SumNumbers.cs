cb4edcd45ba1054fbdd405b8cc4637434f0666af
csibvili <csibi.vili@gmail.com>
2017-09-27 12:12:01 +0200

    feat(test): first failed test created

---
+++ week-04/day-03/Sum/Sum/SumNumbers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum
+{
+    public class SumNumbers
+    {
+        public int SumOfNumbers()
+        {
+            return 0;
+        }
+    }
+}
---
+++ week-04/day-03/Sum/SumTest/SumNumbersTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sum;
+
+namespace SumTest
+{
+    [TestFixture]
+    public class SumNumbersTest
+    {
+        SumNumbers sum = new SumNumbers();
+
+        [Test]
+        public void TestSumNumbersWhenNumberOfInputs1OutputEqualToInput ()
+        {
+            var input = new List<int> { 1 };
+            var output = input;
+
+            Assert.AreEqual(output, sum.SumOfNumbers());
+        }
+    }
+}
