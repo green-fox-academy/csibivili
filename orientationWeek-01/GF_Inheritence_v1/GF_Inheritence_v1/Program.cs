8b3e4a4f6bf105ec0949277e98d838c173b7ed3f
csibvili <csibi.vili@gmail.com>
2017-10-17 13:33:36 +0200

    what?

---
+++ orientationWeek-01/GF_Inheritence_v1/GF_Inheritence_v1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GF_Inheritence_v1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student gohan = new Student();
+            Student gohanTheClone = gohan.Clone();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ orientationWeek-01/GF_Inheritence_v1/GF_Inheritence_v1/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace GF_Inheritence_v1
+{
+    public class Student : ICloneable   
+    {
+
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string PreviousOrganization { get; set; }
+        public int Age { get; set; }
+
+        public void GetGoal()
+        {
+            Console.WriteLine("Be a junior developer or business analyst or devops or...");
+        }
+
+        public object Clone()
+        {
+            return MemberwiseClone();
+        }
+    }
+}
