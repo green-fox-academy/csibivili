8e5d449cdc0e4230a1a946a8dd99b18ce206bbc8
csibvili <csibi.vili@gmail.com>
2017-10-18 13:48:05 +0200

    i will come back here later

---
+++ orientationWeek-01/LINQday/exercise9/exercise9/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a LINQ Expression to convert a char array to a string!
+
+            char[] example = { 'E', 'z', ' ', 'j', 'o', 'l', ' ', 'm', 'e', 'g', 'y', '.' };
+
+            string sentence = new string(example);
+
+            Console.WriteLine(sentence);
+
+            Console.ReadLine();
+        }
+    }
+}
