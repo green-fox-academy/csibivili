19963caaaa9214119931770b55880e1cb2064de7
csibvili <csibi.vili@gmail.com>
2017-10-18 13:18:01 +0200

    exercise 8 is done

---
+++ orientationWeek-01/LINQday/exercise8/exercise8/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace exercise8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a LINQ Expression to find the uppercase characters in a string!
+
+            string example = "Let's find the UPpeRcASe LeTTErs!";
+
+            var uppercaseLetters = from charachter in example
+                                   where char.GetUnicodeCategory(charachter) == UnicodeCategory.UppercaseLetter
+                                   select charachter;
+
+            foreach (var item in uppercaseLetters)
+            {
+                Console.Write(item);
+            }
+
+            Console.WriteLine();
+
+            var upperLetters = example.Where(a => char.GetUnicodeCategory(a) == UnicodeCategory.UppercaseLetter);
+
+            foreach (var item in upperLetters)
+            {
+                Console.Write(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
