76f1aa795b270140fcc3257087d7deaae2396dc1
csibvili <csibi.vili@gmail.com>
2017-10-18 11:57:10 +0200

    exercise 6 is done

---
+++ orientationWeek-01/LINQday/exercise6/exercise6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string example = "How frequent is a character in this question?";
+
+            var frequencyOfChars = from characters in example.ToLower()
+                                   orderby characters ascending
+                                   where !char.IsWhiteSpace(characters)
+                                   group characters by characters into uniqChars                                   
+                                   select new
+                                   {
+                                       uniqChars.Key,
+                                       Count = (from characters in uniqChars select characters).Count()
+                                   };
+
+            foreach (var item in frequencyOfChars)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
