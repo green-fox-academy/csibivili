dd5fa8a89108b9c836da75229b788b02c3bbcaf7
csibvili <csibi.vili@gmail.com>
2017-10-18 11:15:48 +0200

    forth exercise done

---
+++ orientationWeek-01/LINQday/exercise4/exercise4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = new[] { 3, 9, 2, 8, 6, 5 };
+
+            var selectedItems = from a in n
+                                where a * a > 20
+                                orderby a ascending
+                                select a;
+
+            foreach (var item in selectedItems)
+            {
+                Console.Write("{0} ", item);
+            }
+
+            Console.WriteLine();
+
+            var selectedNumbers = n.Where(a => a * a > 20);
+
+            foreach (var item in selectedNumbers)
+            {
+                Console.Write("{0} ", item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
