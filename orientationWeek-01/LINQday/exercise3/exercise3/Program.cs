6c8bd8c66cee130a138eea0a17d0d8bcd4261465
csibvili <csibi.vili@gmail.com>
2017-10-18 11:11:12 +0200

    third exercise done

---
+++ orientationWeek-01/LINQday/exercise3/exercise3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+
+            var squaredValues = from s in n
+                                select s * s;
+
+            foreach (var value in squaredValues)
+            {
+                Console.Write("{0} ",value);
+            }
+
+            Console.WriteLine();
+
+            var sqrValues = n.Select(x => x * x);
+
+            foreach (var value in sqrValues)
+            {
+                Console.Write("{0} ", value);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
