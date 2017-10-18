284914e03c5db55a216611028e713a790bc0f1ef
csibvili <csibi.vili@gmail.com>
2017-10-18 11:06:48 +0200

    second exercise done

---
+++ orientationWeek-01/LINQday/exercise2/exercise2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+
+            var averageOfNumbers = (from a in n
+                                   select a).Average();
+
+            Console.WriteLine(averageOfNumbers);
+
+            var averageOfNums = n.Average();
+
+            Console.WriteLine(averageOfNums);
+
+            Console.ReadLine();
+        }
+    }
+}
