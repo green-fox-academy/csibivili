cfb3c2a7bd261c35f3fbb03cf7440b035999de05
csibvili <csibi.vili@gmail.com>
2017-10-19 11:41:23 +0200

    ParkingLot done

---
+++ orientationWeek-01/EnumDay/ParkingLot/ParkingLot/Car.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingLot
+{
+    public class Car
+    {
+        static readonly Random RANDOM = new Random();
+
+        public string Type, Color;
+
+        public Car()
+        {
+            Type = RandomType();
+            Color = RandomColor();
+        }
+
+        public enum CarType
+        {
+            Skoda,
+            VW,
+            Seat,
+            Porsche,
+            Audi,
+            Lada,
+            BMW,
+            Dacia
+        }
+
+        public enum CarColor
+        {
+            green,
+            red,
+            white,
+            black,
+            blue,
+            yellow,
+            purple,
+            pink
+        }
+
+        public string RandomColor()
+        {
+            Array colorValues = Enum.GetValues(typeof(CarColor));
+            return RandomEnum(colorValues);
+        }
+
+        public string RandomType()
+        {
+            Array typeValues = Enum.GetValues(typeof(CarType));
+            return RandomEnum(typeValues);
+        }
+
+        public string RandomEnum(Array array)
+        {
+            return array.GetValue(RANDOM.Next(array.Length)).ToString();
+        }
+    }
+
+    
+}
---
+++ orientationWeek-01/EnumDay/ParkingLot/ParkingLot/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingLot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Car> cars = new List<Car>();
+
+            for (int i = 0; i < 256; i++)
+            {
+                cars.Add(new Car());
+            }
+
+            var sameType = from car in cars
+                           group car by car.Type into carType
+                           select new
+                           {
+                               carType.Key,
+                               Count = (from car in carType select car).Count()
+                           };
+
+            foreach (var car in sameType)
+            {
+                Console.WriteLine(car);
+            }
+
+            Console.WriteLine();
+
+            var sameColor = from car in cars
+                           group car by car.Color into carColor
+                           select new
+                           {
+                               carColor.Key,
+                               Count = (from car in carColor select car).Count()
+                           };
+
+            foreach (var car in sameColor)
+            {
+                Console.WriteLine(car);
+            }
+
+            Console.WriteLine();
+
+            var MostOccuring = (from car in cars
+                            group car by new { car.Type, car.Color } into mostOccuring                           
+                            orderby mostOccuring.Count() descending
+                            select new
+                            {
+                                mostOccuring.Key,
+                                Count = (from car in mostOccuring select car).Count()
+                            }).Take(1);
+
+            foreach (var car in MostOccuring)
+            {
+                Console.WriteLine(car);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
