4d540471c1baea6f093955988bc86afa5bcd366e
csibvili <csibi.vili@gmail.com>
2017-10-19 12:39:18 +0200

    bone structure created

---
+++ orientationWeek-01/EnumDay/TwentyPlusOneApp/TwentyPlusOneAppTest/TwentyPlusOneTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwentyPlusOneApp;
+
+namespace TwentyPlusOneAppTest
+{
+    [TestFixture]
+    public class TwentyPlusOneTest
+    {
+        [Test]
+        public void SmokeTestForConstructor()
+        {
+            Deck deck = new Deck();
+        }
+    }
+}
