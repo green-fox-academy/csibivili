e2f3dd7d1ccc6ab6d2705a62d85e42c398e58f97
csibvili <csibi.vili@gmail.com>
2017-10-24 15:03:06 +0200

    hello world it is a html page

---
+++ orientationWeek-01/.ASP.NET.core/HelloWorld/HelloWorld/Controllers/WebController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using HelloWorld.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HelloWorld.Controllers
+{
+    [Route("web")]
+    public class WebController : Controller
+    {
+        [Route("greeting")]
+        public IActionResult Greeting()
+        {
+            var greeting = new Greeting()
+            {
+                ID = 1,
+                Content = "World"
+            };
+
+            return View(greeting);
+        }
+
+    }
+}
