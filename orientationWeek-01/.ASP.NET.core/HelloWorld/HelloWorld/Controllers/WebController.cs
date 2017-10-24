70d6fa4a2ff3af0fe596d16f667246f2fcd38950
csibvili <csibi.vili@gmail.com>
2017-10-24 16:13:32 +0200

    no further idea

--- orientationWeek-01/.ASP.NET.core/HelloWorld/HelloWorld/Controllers/WebController.cs
+++ orientationWeek-01/.ASP.NET.core/HelloWorld/HelloWorld/Controllers/WebController.cs
@@ -12,16 +12,23 @@ namespace HelloWorld.Controllers
     [Route("web")]
     public class WebController : Controller
     {
+        string[] hellos = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
+                "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
+                "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
+                "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
+
+
         [Route("greeting")]
         public IActionResult Greeting()
         {
-            var greeting = new Greeting()
-            {
-                ID = 1,
-                Content = "World"
-            };
+            var greetings = new List<Greeting>();
 
-            return View(greeting);
+            foreach (var hello in hellos)
+            {
+                greetings.Add(new Greeting(hello));
+            }    
+
+            return View(greetings);
         }
 
     }
