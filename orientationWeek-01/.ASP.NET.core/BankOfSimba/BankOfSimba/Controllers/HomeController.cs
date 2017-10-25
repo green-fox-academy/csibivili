5c4dd4d6f4cd15b80cf237c48cdd657d80bbbc37
csibvili <csibi.vili@gmail.com>
2017-10-25 16:10:51 +0200

    i have no idea what i am doing

--- orientationWeek-01/.ASP.NET.core/BankOfSimba/BankOfSimba/Controllers/HomeController.cs
+++ orientationWeek-01/.ASP.NET.core/BankOfSimba/BankOfSimba/Controllers/HomeController.cs
@@ -12,17 +12,6 @@ namespace BankOfSimba.Controllers
         {
             var homeViewModel = new HomeViewModel();
 
-            homeViewModel.bankAccount.Add(new Models.BankAccount
-            { Name = "Simba", Balance = 2000.00, AnimalType = Models.TypeOfAnimal.Lion });
-            homeViewModel.bankAccount.Add(new Models.BankAccount
-            { Name = "Mufasa", Balance = 3000000.00, AnimalType = Models.TypeOfAnimal.Lion, isKing = true });
-            homeViewModel.bankAccount.Add(new Models.BankAccount
-            { Name = "Rafiki", Balance = 20.00, AnimalType = Models.TypeOfAnimal.Monkey });
-            homeViewModel.bankAccount.Add(new Models.BankAccount
-            { Name = "Zordon", Balance = -2000000.00, AnimalType = Models.TypeOfAnimal.Lion, isGood = false });
-            homeViewModel.bankAccount.Add(new Models.BankAccount
-            { Name = "Zazu", Balance = -20000.00, AnimalType = Models.TypeOfAnimal.Parrot });
-
             for (int i = 0; i < homeViewModel.bankAccount.Count(); i++)
             {
                 if (name == homeViewModel.bankAccount[i].Name)
--- orientationWeek-01/.ASP.NET.core/BankOfSimba/BankOfSimba/ViewModels/Home/HomeViewModel.cs
+++ orientationWeek-01/.ASP.NET.core/BankOfSimba/BankOfSimba/ViewModels/Home/HomeViewModel.cs
@@ -10,5 +10,19 @@ namespace BankOfSimba.ViewModels.Home
         {
             bankAccount = new List<BankAccount>();
         }
+
+        public void Fill()
+        {
+            bankAccount.Add(new Models.BankAccount
+            { Name = "Simba", Balance = 2000.00, AnimalType = Models.TypeOfAnimal.Lion });
+            bankAccount.Add(new Models.BankAccount
+            { Name = "Mufasa", Balance = 3000000.00, AnimalType = Models.TypeOfAnimal.Lion, isKing = true });
+            bankAccount.Add(new Models.BankAccount
+            { Name = "Rafiki", Balance = 20.00, AnimalType = Models.TypeOfAnimal.Monkey });
+            bankAccount.Add(new Models.BankAccount
+            { Name = "Zordon", Balance = -2000000.00, AnimalType = Models.TypeOfAnimal.Lion, isGood = false });
+            bankAccount.Add(new Models.BankAccount
+            { Name = "Zazu", Balance = -20000.00, AnimalType = Models.TypeOfAnimal.Parrot });
+        }
     }
 }
