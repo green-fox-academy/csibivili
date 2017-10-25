addede661177b917bbb9b67e8ad95cadaeebb1d2
csibvili <csibi.vili@gmail.com>
2017-10-25 16:19:19 +0200

    problem is the same

--- orientationWeek-01/.ASP.NET.core/BankOfSimba/BankOfSimba/ViewModels/Home/HomeViewModel.cs
+++ orientationWeek-01/.ASP.NET.core/BankOfSimba/BankOfSimba/ViewModels/Home/HomeViewModel.cs
@@ -8,21 +8,23 @@ namespace BankOfSimba.ViewModels.Home
         public List<BankAccount> bankAccount;
         public HomeViewModel()
         {
-            bankAccount = new List<BankAccount>();
+            bankAccount = Fill();
         }
 
-        public void Fill()
+        public List<BankAccount> Fill()
         {
-            bankAccount.Add(new Models.BankAccount
+            List<BankAccount> temp = new List<BankAccount>();
+            temp.Add(new Models.BankAccount
             { Name = "Simba", Balance = 2000.00, AnimalType = Models.TypeOfAnimal.Lion });
-            bankAccount.Add(new Models.BankAccount
+            temp.Add(new Models.BankAccount
             { Name = "Mufasa", Balance = 3000000.00, AnimalType = Models.TypeOfAnimal.Lion, isKing = true });
-            bankAccount.Add(new Models.BankAccount
+            temp.Add(new Models.BankAccount
             { Name = "Rafiki", Balance = 20.00, AnimalType = Models.TypeOfAnimal.Monkey });
-            bankAccount.Add(new Models.BankAccount
+            temp.Add(new Models.BankAccount
             { Name = "Zordon", Balance = -2000000.00, AnimalType = Models.TypeOfAnimal.Lion, isGood = false });
-            bankAccount.Add(new Models.BankAccount
+            temp.Add(new Models.BankAccount
             { Name = "Zazu", Balance = -20000.00, AnimalType = Models.TypeOfAnimal.Parrot });
+            return temp;
         }
     }
 }
