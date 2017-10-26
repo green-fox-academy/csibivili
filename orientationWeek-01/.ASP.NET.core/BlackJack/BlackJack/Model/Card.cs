6b681157df9be4607e3d876e7441019f51cd3dc8
csibvili <csibi.vili@gmail.com>
2017-10-26 19:41:26 +0200

    card and deck classes copied from last week

--- orientationWeek-01/.ASP.NET.core/BlackJack/BlackJack/Model/Card.cs
+++ orientationWeek-01/.ASP.NET.core/BlackJack/BlackJack/Model/Card.cs
@@ -5,7 +5,74 @@ using System.Threading.Tasks;
 
 namespace BlackJack.Model
 {
-    public class Card
+    public class Card : IComparable
     {
+        public Suit CardSuit;
+        public Rank CardRank;
+
+        public Card(Suit suit, Rank rank)
+        {
+            CardSuit = suit;
+            CardRank = rank;
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (obj != null)
+            {
+                return SameSuit(obj) + SameRank(obj);
+            }
+            return 1;
+        }
+
+        public int SameSuit(object obj)
+        {
+            Card otherCard = obj as Card;
+            if (otherCard != null)
+            {
+                return this.CardSuit.CompareTo(otherCard.CardSuit);
+            }
+            return 1;
+        }
+
+        public int SameRank(object obj)
+        {
+            Card otherCard = obj as Card;
+            if (otherCard != null)
+            {
+                return this.CardRank.CompareTo(otherCard.CardRank);
+            }
+            return 1;
+        }
+
+        public string GetInfo()
+        {
+            return String.Format("{0} of {1}", CardRank, CardSuit);
+        }
+
+        public enum Suit
+        {
+            Spades,
+            Hearts,
+            Clubs,
+            Diamonds,
+        }
+
+        public enum Rank
+        {
+            Two,
+            Three,
+            Four,
+            Five,
+            Six,
+            Seven,
+            Eight,
+            Nine,
+            Ten,
+            Jack,
+            Queen,
+            King,
+            Ace
+        }
     }
 }
---
+++ orientationWeek-01/.ASP.NET.core/BlackJack/BlackJack/Model/Deck.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlackJack.Model
+{
+    public class Deck
+    {
+        static readonly Random RANDOM = new Random();
+
+        public List<Card> Cards;
+
+        public Deck()
+        {
+            Cards = FillDeck();
+        }
+
+        private List<Card> FillDeck()
+        {
+            var deck = (from suit in (Card.Suit[])Enum.GetValues(typeof(Card.Suit))
+                        from rank in (Card.Rank[])Enum.GetValues(typeof(Card.Rank))
+                        select new Card(suit, rank)).ToList<Card>();
+            return deck;
+        }
+
+        public static List<Card> ShuffleDeck(List<Card> cards)
+        {
+            for (int i = cards.Count - 1; i > 0; i--)
+            {
+                int n = RANDOM.Next(i + 1);
+                Card tmp = cards[n];
+                cards[n] = cards[i];
+                cards[i] = tmp;
+            }
+            return cards;
+        }
+
+        public static List<Card> RankSort(List<Card> cards)
+        {
+            return (from card in cards
+                    orderby card.CardRank
+                    select card).ToList();
+        }
+
+        public static Card PullFirst(List<Card> cards)
+        {
+            Card pulled = cards[0];
+            cards.Remove(pulled);
+            return pulled;
+        }
+
+        public static Card PullLast(List<Card> cards)
+        {
+            Card pulled = cards[cards.Count() - 1];
+            cards.Remove(pulled);
+            return pulled;
+        }
+
+        public static Card PullRandom(List<Card> cards)
+        {
+            Card pulled = cards[RANDOM.Next(cards.Count())];
+            cards.Remove(pulled);
+            return pulled;
+        }
+    }
+}
