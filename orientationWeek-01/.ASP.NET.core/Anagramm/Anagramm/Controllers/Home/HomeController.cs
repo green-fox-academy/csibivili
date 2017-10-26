1a871bacbf9fa14eff55f4d49442165adcc9863d
csibvili <csibi.vili@gmail.com>
2017-10-26 16:45:46 +0200

    feat(Anagramm): it saves the inputs in a list

---
+++ orientationWeek-01/.ASP.NET.core/Anagramm/Anagramm/Controllers/Home/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Anagramm.Model;
+using System.Collections.Generic;
+
+namespace Anagramm.Controllers.Home
+{
+    public class HomeController : Controller
+    {
+        Word word;
+        List<Word> wordsList;
+
+        public HomeController(Word word, List<Word> wordsList)
+        {
+            this.word = word;
+            this.wordsList = wordsList;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Word word)
+        {
+            wordsList.Add(word);
+            return RedirectToAction("result", word);
+        }
+
+        [HttpGet]
+        [Route("result")]
+        public IActionResult Result(Word word)
+        {
+            return View(word);
+        }
+    }
+}
---
+++ orientationWeek-01/.ASP.NET.core/Anagramm/Anagramm/Model/Word.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Anagramm.Model
+{
+    public class Word
+    {
+        public string InputWord { get; set; }
+    }
+}
