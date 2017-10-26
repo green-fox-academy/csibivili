de9966a5fc057a71ea732b435e261fc9f4315345
csibvili <csibi.vili@gmail.com>
2017-10-26 15:58:21 +0200

    this app will greet the user

---
+++ orientationWeek-01/.ASP.NET.core/GreetSomeone/GreetSomeone/Controllers/Home/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using GreetSomeone.Model;
+
+namespace GreetSomeone.Controllers.Home
+{
+    public class HomeController : Controller
+    {
+        Person person;
+
+        public HomeController(Person person)
+        {
+            this.person = person;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Person person)
+        {
+            return RedirectToAction("greet", person);
+        }
+
+        [HttpGet]
+        [Route("greet")]
+        public IActionResult Greet(Person person)
+        {
+            return View(person);
+        }
+    }
+}
