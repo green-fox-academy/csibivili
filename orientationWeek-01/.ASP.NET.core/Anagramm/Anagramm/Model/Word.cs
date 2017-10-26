47ecf8331b7b479927db1b467f4886f493a74d4c
csibvili <csibi.vili@gmail.com>
2017-10-26 17:01:26 +0200

    feat(anagramm): anagram comparison function added

--- orientationWeek-01/.ASP.NET.core/Anagramm/Anagramm/Model/Word.cs
+++ orientationWeek-01/.ASP.NET.core/Anagramm/Anagramm/Model/Word.cs
@@ -8,5 +8,15 @@ namespace Anagramm.Model
     public class Word
     {
         public string InputWord { get; set; }
+
+        public bool Compare(Word word1, Word word2)
+        {
+            return Converter(word1).Equals(Converter(word2));
+        }
+
+        public string Converter(Word word)
+        {           
+            return word.InputWord.OrderBy(x => x).ToString();
+        }
     }
 }
