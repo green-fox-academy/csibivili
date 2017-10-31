c0d5659b72c7030d409c5d88eb0a235c0316d92a
csibvili <csibi.vili@gmail.com>
2017-10-31 13:21:11 +0100

    first controller ready

---
+++ orientaionWeek-03/ToDoWebApp/ToDoWebApp/Controllers/TodoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoWebApp.Controllers
+{
+    [Route("/Todo")]
+    public class TodoController : Controller
+    {
+        [Route("")]
+        [HttpGet]
+        public IActionResult List()
+        {
+            string tmp = "This is my first todo";
+            return View((object)tmp);
+        }
+    }
+}
