0f2ac4640155318a9360d7ac3308e7358efdcf79
csibvili <csibi.vili@gmail.com>
2017-10-31 15:04:57 +0100

    some todos added in the controller, view changed accordingly

--- orientaionWeek-03/ToDoWebApp/ToDoWebApp/Controllers/TodoController.cs
+++ orientaionWeek-03/ToDoWebApp/ToDoWebApp/Controllers/TodoController.cs
@@ -20,7 +20,9 @@ namespace ToDoWebApp.Controllers
         [Route("")]
         [HttpGet]
         public IActionResult List()
-        {           
+        {
+            TodoRepository.AddTodo(new Models.Todo() { Title = "move forward", IsDone = false, IsUrgent = true });
+            TodoRepository.AddTodo(new Models.Todo() { Title = "add more elements", IsDone = true, IsUrgent = false });
             return View(TodoRepository.GetList());
         }
     }
