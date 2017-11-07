ccb55c00e6983a11719e3c24739fdfaa1ee9c0dc
csibvili <csibi.vili@gmail.com>
2017-11-07 17:23:32 +0100

    public static readonly variable used dictionary like

---
+++ orientation-Week-04/ArrayHandler/ArrayHandler/Collections/Operations.cs
@@ -0,0 +1,15 @@
+﻿using ArrayHandler.Models;
+using System.Collections.Generic;
+
+namespace ArrayHandler.Collections
+{
+    public class Operations
+    {
+        public static readonly List<IOperation> OperationTypes = new List<IOperation>()
+        {
+            new Sum(),
+            new Multiply(),
+            new Doubling()
+        };
+    }
+}
