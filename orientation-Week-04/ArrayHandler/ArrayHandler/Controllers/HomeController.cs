ced8732aa31b1d5874c979b3116acfa6548778fa
csibvili <csibi.vili@gmail.com>
2017-11-07 17:24:50 +0100

    choose the right operation and get result

---
+++ orientation-Week-04/ArrayHandler/ArrayHandler/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using ArrayHandler.Collections;
+using ArrayHandler.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArrayHandler.Controllers
+{
+    [Route("/")]
+    public class HomeController : Controller
+    {
+        [HttpPost]
+        [Route("/arrays")]
+        public IActionResult Arrays([FromBody]OperationOnNumbers operationOnNumbers)
+        {
+            Operations.OperationTypes
+                .Find(operation => operation.WhatOperation == operationOnNumbers.What)
+                .Result = operationOnNumbers.Numbers;
+
+            object result = Operations.OperationTypes
+                .Find(operation => operation.WhatOperation == operationOnNumbers.What)
+                .Result;
+
+            return Json(new { result = result });
+        }
+    }
+}
