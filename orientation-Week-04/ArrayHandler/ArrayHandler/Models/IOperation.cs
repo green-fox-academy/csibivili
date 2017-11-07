fb45151577753bf809ddede65e0ee516c266e877
csibvili <csibi.vili@gmail.com>
2017-11-07 17:20:56 +0100

    interface with two properties: whatOperation and result, sum/multiply/double class created

---
+++ orientation-Week-04/ArrayHandler/ArrayHandler/Models/IOperation.cs
@@ -0,0 +1,45 @@
+﻿namespace ArrayHandler.Models
+{
+    public interface IOperation
+    {
+        string WhatOperation { get; set; }
+        object Result { get; set; }
+    }
+
+    public class Sum : IOperation
+    {
+        private int sum;
+
+        public string WhatOperation { get; set; } = "sum";
+        public object Result
+        {
+            get { return sum; }
+            set { sum = OperationOnNumbers.Sum((int[])value); }
+        }
+    }
+
+    public class Multiply : IOperation
+    {
+        private int multiplied;
+
+        public string WhatOperation { get; set; } = "multiply";
+        public object Result
+        {
+            get { return multiplied; }
+            set { multiplied = OperationOnNumbers.Multiply((int[])value); }
+        }
+    }
+
+    public class Doubling : IOperation
+    {
+        private int[] doubled;
+
+        public string WhatOperation { get; set; } = "double";
+        public object Result
+        {
+            get { return doubled; }
+            set { doubled = OperationOnNumbers.Double((int[])value); }
+        }
+    }
+
+}
---
+++ orientation-Week-04/ArrayHandler/ArrayHandler/Models/OperationOnNumbers.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+
+namespace ArrayHandler.Models
+{
+    public class OperationOnNumbers
+    {
+        public string What { get; set; }
+        public int[] Numbers { get; set; }
+
+        public static int Sum(int[] numbers)
+        {
+            return numbers.Sum();
+        }
+
+        public static int Multiply(int[] numbers)
+        {
+            int multiplied = 1;
+            foreach (var number in numbers)
+            {
+                multiplied *= number;
+            }
+            return multiplied;
+        }
+
+        public static int[] Double(int[] numbers)
+        {
+            int[] doubled = new int[numbers.Length];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                doubled[i] = numbers[i] * 2;
+            }
+            return doubled;
+        }
+    }
+}
